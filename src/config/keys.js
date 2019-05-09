9234dbd8ef38688f06a5b635c39c69a8f475bcb4
João Marcos <38922264+dunderjm@users.noreply.github.com>
2019-05-09 01:04:08 -0300

    Delete keys.js

--- src/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoDB: "mongodb+srv://guaribati:guaribatibiblib@cluster0-pgacx.mongodb.net/test?retryWrites=true",
-    googleClientID: "",
-    googleClientIDSecret: "",
-    goodReadskey: "2Dod2Bovub0gcqMOjfJFA",
-    googleBooksAPi: "AIzaSyB2gc1mmsHcYQHzazmQQweQNresujWqn1w",
-}
