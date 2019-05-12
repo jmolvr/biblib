e21d7e1be125d479b99a4eda8e955af18690b159
João Marcos <38922264+dunderjm@users.noreply.github.com>
2019-05-11 22:00:06 -0300

    Login com google #1
    Login com google implementado usando passportjs e preparação para uploado ao Heroku.

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const passport = require('passport');
-const auth_routes = express.Router();
-auth_routes.use(passport.authenticate('jwt', {session:false}));
-auth_routes.get('/books', (req, res) => {
-    //Função para receber dados dos livros.
-    res.json({Mensagem: "Oi. Esta função funciona?"});
-})
-auth_routes.post('/books', (req, res) => {
-    //Função para enviar dados dos livros.
-    res.json({Mensagem: "A"});
-})
-auth_routes.put('/books', (req, res) => {
-    //Função para atualizar livros.
-    res.json({Mensagem: "B"});
-})
-auth_routes.get('/user', (req, res) => {
-    res.json({Mensagem: "C"});
-})
-auth_routes.put('/user', (req, res) => {
-    res.json({Mensagem: "D"});
-})
-module.exports = auth_routes;
+const router = express.Router();
+const AuthController = require('../controller/userController');
+
+router.post("/register", AuthController.register);
+
+router.get("/login", AuthController.login);
+
+router.get("/logout");
+router.get("/google", passport.authenticate('google', {
+    session: false,
+    scope: ['profile', 'email']
+}));
+
+//callback for google strategy
+router.get('/google/redirect',AuthController.googleStrategy);
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const keys = require('./config/keys');
-const passport = require('passport');
-require('./config/passport');
+require('./config/passport-setup');
+
 const app = express();
 
-mongoose.connect(keys.mongoDB, { useNewUrlParser: true})
+mongoose.connect(process.env.mongoDB || keys.mongoDB, { useNewUrlParser: true})
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
-//app.use(passport.initialize());
+
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(require('./routes/routes'));
 app.use('/auth',require('./routes/auth_routes.js'));
-app.listen(3333);
+
+app.use(require('./routes/routes'));
+
+app.listen(process.env.PORT || 3333);
